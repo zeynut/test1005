56a1c778998daa6031396e6a857ece9c670132a6
zeynut <zeynut@gmail.com>
2019-10-05 14:03:44 +0900

    js 화일 만들곻 버튼 구현

---
+++ components/Masteet.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Masteet extends Component {
+    render() {
+        return (
+            <div>
+                <button>aaaaa</button>
+            </div>
+        )
+    }
+}
